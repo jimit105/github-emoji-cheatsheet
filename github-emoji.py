8abe748a7a6efa76d3b000c35d87f29ad463176f
Jimit Dholakia <jimitd101@gmail.com>
2026-02-10 01:56:32 -0800

    Add Jekyll front matter to index.md output

--- github-emoji.py
+++ github-emoji.py
@@ -9,6 +9,14 @@ r.raise_for_status()
 
 emojis = r.json()
 
+# Jekyll front matter
+front_matter = '''---
+layout: default
+title: GitHub Emoji Cheatsheet
+---
+
+'''
+
 action_badge = '[![GitHub Emoji Update](https://github.com/jimit105/github-emoji-cheatsheet/workflows/GitHub%20Emoji%20Update/badge.svg?branch=master)](https://github.com/jimit105/github-emoji-cheatsheet/actions)'
 header = f'## GitHub Emoji Cheatsheet\n\n{action_badge}\n\n'
 table = '|Icon|Emoji Code|\n|---|---|\n'
@@ -17,6 +25,6 @@ for key in emojis.keys():
     table += f'|:{key}:|`:{key}:`|\n'
 
 with open('index.md', 'w', encoding='utf-8') as f:
-    f.write(header + table)
+    f.write(front_matter + header + table)
 
 print('Emoji Update Complete')
